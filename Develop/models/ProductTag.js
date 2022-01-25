8f00ae0df88a253f1720aec4dfeecc34f1e5d379
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-01-24 23:58:54 -0700

    Updated all models files by defining the columns and adding association methods.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
@@ -18,3 +40,5 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+//JOIN TABLE: will receive both a product id and Tag id
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-// Categories have many Products
+Product.belongsTo(Category, {
+  through: {
+    model: Category,
+    unique: false
+  },
+  // Categories have many Products
+  as: 'product_category'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
